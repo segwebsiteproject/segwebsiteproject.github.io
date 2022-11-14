0bc874b5f73df84df33ee106b7828887e864680e
gtouk <gtouk010@uottawa.ca>
2022-11-14 10:22:12 -0500

    hello

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-<<<<<<< HEAD
+HEAD
 function text() {
   document.getElementById("Partner").style.display="block";
-=======
+}
+
 function myFunction() {
   let image = document.getElementById("AppStore");
  
   image.src ="Pictures/best.png"
 
   document.getElementById("butid")
->>>>>>> d399e84653c5d23753fd0b1702e8fae44da5e6e3
-}
+
+} 
